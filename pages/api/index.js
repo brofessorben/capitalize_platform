1706588899ff6018d5c547f70c81b20e0c57bf8b
brofessorben <bengg2125@gmail.com>
2025-08-31 01:21:13 -0500

    Update index.js

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,3 +1,8 @@
-export default function Placeholder() {
-  return null;
+export default function handler(req, res) {
+  if (req.method !== "GET") {
+    res.setHeader("Allow", "GET");
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+  
+  return res.status(200).json({ message: "CAPITALIZE API is running" });
 }
