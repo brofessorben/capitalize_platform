b110d2f68db9e3e9eeaefde9cff597d92304d3bd
brofessorben <bengg2125@gmail.com>
2025-09-01 00:53:36 -0500

    Update page.jsx

--- app/(dash)/referrer/page.jsx
+++ app/(dash)/referrer/page.jsx
@@ -1,19 +1,15 @@
-"use client";
-
 import BackButton from "../../components/BackButton";
 
 export default function ReferrerDash() {
   return (
-    <div className="p-6 space-y-6">
+    <div className="p-6 space-y-8">
       <div className="flex items-center justify-between">
         <h1 className="text-2xl font-bold">Referrer Dashboard</h1>
         <BackButton />
       </div>
 
       <div className="rounded-2xl border p-4">
-        <p className="text-gray-600">
-          Your referred leads will show here. Submit a lead and track its progress.
-        </p>
+        Your referred leads will show here.
       </div>
     </div>
   );
