d7dbe83307112f719b5089fc14d61e15558c080d
brofessorben <bengg2125@gmail.com>
2025-09-04 02:40:52 -0500

    Update page.jsx

--- app/(dash)/referrer/page.jsx
+++ app/(dash)/referrer/page.jsx
@@ -1,6 +1,10 @@
-"use client";
-import AIChatPage from "../../AIChatPage.jsx";
+import AIChatPage from '../../components/AIChatPage';
 
 export default function ReferrerDash() {
-  return <AIChatPage role="referrer" header="Referrer Console" />;
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Referrer Dashboard</h1>
+      <AIChatPage />
+    </div>
+  );
 }
