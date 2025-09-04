ab34124820f82989f794fe33430228ac6c1ecfc2
brofessorben <bengg2125@gmail.com>
2025-09-04 03:34:44 -0500

    Update page.jsx

--- app/(dash)/host/page.jsx
+++ app/(dash)/host/page.jsx
@@ -1,10 +1,8 @@
 import AIChatPage from "../../components/AIChatPage";
-
 export const metadata = { title: "Host Dashboard" };
-
 export default function HostDash() {
   return (
-    <div className="min-h-screen p-6 bg-gray-50">
+    <div className="min-h-screen p-6 bg-neutral-900">
       <AIChatPage role="host" header="Host Console" />
     </div>
   );
