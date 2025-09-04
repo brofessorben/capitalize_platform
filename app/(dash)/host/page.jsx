bc1aa0909122c75ebd67270eb1495ddab4a6e6e6
brofessorben <bengg2125@gmail.com>
2025-09-04 02:27:41 -0500

    Update page.jsx

--- app/(dash)/host/page.jsx
+++ app/(dash)/host/page.jsx
@@ -1,21 +1,6 @@
-- import AIChatPage from '../../components/AIChatPage'
-+ import AIChatPage from '../../components/AIChatPage.jsx'
+"use client";
+import AIChatPage from "../../components/AIChatPage.jsx";
 
 export default function HostDash() {
-  return (
-    <AIChatPage
-      role="host"
-      title="Host Command Center"
-      kpis={[
-        { label: "Requests", value: "1" },
-        { label: "Proposals Received", value: "0" },
-        { label: "Booked", value: "0" },
-      ]}
-      quickPrompts={[
-        "Create a new request",
-        "Compare proposals",
-        "What should I ask vendors?",
-      ]}
-    />
-  );
+  return <AIChatPage role="host" header="Host Console" />;
 }
