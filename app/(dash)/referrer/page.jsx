cdaad0986a6516f36633d84722d22d4cd8fcfb50
brofessorben <bengg2125@gmail.com>
2025-09-04 03:15:39 -0500

    Update page.jsx

--- app/(dash)/referrer/page.jsx
+++ app/(dash)/referrer/page.jsx
@@ -5,7 +5,7 @@ export const metadata = { title: "Referrer Dashboard" };
 export default function ReferrerDash() {
   return (
     <div className="min-h-screen p-6 bg-gray-50">
-      <AIChatPage header="Referrer Console" />
+      <AIChatPage role="referrer" header="Referrer Console" />
     </div>
   );
 }
