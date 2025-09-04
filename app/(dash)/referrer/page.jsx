668ee125da72ab066c777ecaeed3a7d7e45c1342
brofessorben <bengg2125@gmail.com>
2025-09-04 03:34:25 -0500

    Update page.jsx

--- app/(dash)/referrer/page.jsx
+++ app/(dash)/referrer/page.jsx
@@ -1,7 +1,5 @@
 import AIChatPage from "../../components/AIChatPage";
-
 export const metadata = { title: "Referrer Dashboard" };
-
 export default function ReferrerDash() {
   return (
     <div className="min-h-screen p-6 bg-neutral-900">
