18e50b7f28670a88545a5908405c67941372b3e6
brofessorben <bengg2125@gmail.com>
2025-09-04 02:28:33 -0500

    Update page.jsx

--- app/(dash)/referrer/page.jsx
+++ app/(dash)/referrer/page.jsx
@@ -1,21 +1,6 @@
-- import AIChatPage from '../../components/AIChatPage'
-+ import AIChatPage from '../../components/AIChatPage.jsx'
+"use client";
+import AIChatPage from "../../components/AIChatPage.jsx";
 
 export default function ReferrerDash() {
-  return (
-    <AIChatPage
-      role="referrer"
-      title="Referrer Command Center"
-      kpis={[
-        { label: "Open Leads", value: "3" },
-        { label: "Booked", value: "1" },
-        { label: "Rewards", value: "$120.00" },
-      ]}
-      quickPrompts={[
-        "Start a new referral",
-        "Draft outreach for this host",
-        "What’s the status of my leads?",
-      ]}
-    />
-  );
+  return <AIChatPage role="referrer" header="Referrer Console" />;
 }
