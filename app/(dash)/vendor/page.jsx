bcfe17fa0054df98e3caac9cfa754410e6fb16bb
brofessorben <bengg2125@gmail.com>
2025-09-04 02:28:07 -0500

    Update page.jsx

--- app/(dash)/vendor/page.jsx
+++ app/(dash)/vendor/page.jsx
@@ -1,21 +1,6 @@
-- import AIChatPage from '../../components/AIChatPage'
-+ import AIChatPage from '../../components/AIChatPage.jsx'
+"use client";
+import AIChatPage from "../../components/AIChatPage.jsx";
 
 export default function VendorDash() {
-  return (
-    <AIChatPage
-      role="vendor"
-      title="Vendor Command Center"
-      kpis={[
-        { label: "Open Leads", value: "2" },
-        { label: "Proposals Sent", value: "5" },
-        { label: "Payouts", value: "$0.00" },
-      ]}
-      quickPrompts={[
-        "Draft a catering proposal",
-        "Help me price per person",
-        "Negotiate this reply",
-      ]}
-    />
-  );
+  return <AIChatPage role="vendor" header="Vendor Console" />;
 }
