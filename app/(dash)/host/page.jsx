e4d7522288684c82a5dacf51a23df38f6fab5a3a
brofessorben <bengg2125@gmail.com>
2025-09-04 03:15:58 -0500

    Update page.jsx

--- app/(dash)/host/page.jsx
+++ app/(dash)/host/page.jsx
@@ -5,7 +5,7 @@ export const metadata = { title: "Host Dashboard" };
 export default function HostDash() {
   return (
     <div className="min-h-screen p-6 bg-gray-50">
-      <AIChatPage header="Host Console" />
+      <AIChatPage role="host" header="Host Console" />
     </div>
   );
 }
