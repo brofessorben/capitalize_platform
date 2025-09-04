2988d59d862e5f15c71a10c9d34d628f3b606349
brofessorben <bengg2125@gmail.com>
2025-09-04 05:38:15 -0500

    Update page.jsx

--- app/(dash)/referrer/page.jsx
+++ app/(dash)/referrer/page.jsx
@@ -1,5 +1,5 @@
 // app/(dash)/referrer/page.jsx
-import AIChatPage from "@/app/components/AIChatPage";
+import AIChatPage from "../../components/AIChatPage";
 
 export default function ReferrerDash() {
   return (
