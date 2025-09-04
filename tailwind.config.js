d3e1376dff6e6d47eb1f2b37ae73d7b65e144bfa
brofessorben <bengg2125@gmail.com>
2025-09-04 02:53:47 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,16 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: { extend: {} },
-  plugins: [require("@tailwindcss/typography")],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [],
 };
