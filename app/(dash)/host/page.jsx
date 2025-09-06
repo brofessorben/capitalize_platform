310452a7385b406023336d93457385933aeed1c4
brofessorben <bengg2125@gmail.com>
2025-09-06 02:11:58 -0500

    Update page.jsx

--- app/(dash)/host/page.jsx
+++ app/(dash)/host/page.jsx
@@ -1,20 +1,16 @@
-// app/(dash)/host/page.jsx
 "use client";
-import dynamic from "next/dynamic";
-import EventList from "@/app/components/eventlist";
 
-const AIChatPage = dynamic(() => import("@/app/components/aichatpage"), { ssr: false });
+import dynamic from "next/dynamic";
+
+const AIChatPage = dynamic(() => import("../../components/aichatpage"), { ssr: false });
+const EventList   = dynamic(() => import("../../components/eventlist"),   { ssr: false });
 
 export default function HostDash() {
   return (
-    <div className="grid lg:grid-cols-3 gap-6">
-      <div className="lg:col-span-2">
-        <AIChatPage role="host" header="Host Console" />
-      </div>
-      <div>
-        <h3 className="text-zinc-100 font-semibold mb-3">Your Events</h3>
-        <EventList roleFilter="host" />
-      </div>
+    <div className="max-w-6xl mx-auto px-4 pb-20">
+      <AIChatPage role="host" header="Host Console" />
+      <div className="mt-10" />
+      <EventList />
     </div>
   );
 }
