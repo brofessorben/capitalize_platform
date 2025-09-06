7bbce1f41000bd9a4419d4dc8caf6422bd7a711d
brofessorben <bengg2125@gmail.com>
2025-09-05 19:29:56 -0500

    Update page.jsx

--- app/(dash)/vendor/page.jsx
+++ app/(dash)/vendor/page.jsx
@@ -1,19 +1,9 @@
 "use client";
 
 import dynamic from "next/dynamic";
+
 const AIChatPage = dynamic(() => import("../../components/aichatpage"), { ssr: false });
-const EventList  = dynamic(() => import("../../components/eventlist"),  { ssr: false });
 
 export default function VendorDash() {
-  const events = [
-    { id: "proposal-amy-wedding", title: "Proposal • Amy’s Wedding", date: "Oct 17, 2025" },
-    { id: "proposal-corp-holiday", title: "Proposal • Corp Holiday", date: "Dec 12, 2025" },
-  ];
-
-  return (
-    <div className="space-y-6">
-      <AIChatPage role="vendor" header="Vendor Console" />
-      <EventList events={events} />
-    </div>
-  );
+  return <AIChatPage role="vendor" header="Vendor Console" />;
 }
