6dcf8c93ad90571a3fa543d61c7441a8c86ee747
brofessorben <bengg2125@gmail.com>
2025-09-05 19:16:37 -0500

    Update page.jsx

--- app/(dash)/referrer/page.jsx
+++ app/(dash)/referrer/page.jsx
@@ -1,15 +1,19 @@
 "use client";
 
 import dynamic from "next/dynamic";
-
 const AIChatPage = dynamic(() => import("../../components/aichatpage"), { ssr: false });
-const EventList = dynamic(() => import("../../components/eventlist"), { ssr: false });
+const EventList  = dynamic(() => import("../../components/eventlist"),  { ssr: false });
 
 export default function ReferrerDash() {
+  const events = [
+    { id: "lead-amy-wedding", title: "Lead • Amy’s Wedding", date: "Oct 17, 2025" },
+    { id: "lead-corp-holiday", title: "Lead • Corp Holiday", date: "Dec 12, 2025" },
+  ];
+
   return (
     <div className="space-y-6">
-      <AIChatPage role="referrer" header="Referrer Console" />
-      <EventList events={[{ title: "Lead for Amy’s Wedding", date: "Oct 17, 2025" }]} />
+      <AIChatPage role="referrer" header="Referrer Console" showLeadForm />
+      <EventList events={events} />
     </div>
   );
 }
