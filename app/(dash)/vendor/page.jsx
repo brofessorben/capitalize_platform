eca79bda9fc6e52b0436f7c7e860f7f1067df085
brofessorben <bengg2125@gmail.com>
2025-09-06 02:32:04 -0500

    Update page.jsx

--- app/(dash)/vendor/page.jsx
+++ app/(dash)/vendor/page.jsx
@@ -2,15 +2,18 @@
 
 import dynamic from "next/dynamic";
 
-const AIChatPage = dynamic(() => import("../../components/aichatpage"), { ssr: false });
-const EventList   = dynamic(() => import("../../components/eventlist"),   { ssr: false });
+const AIChatPage = dynamic(() => import("../../components/aichatpage"), {
+  ssr: false,
+});
+const EventList = dynamic(() => import("../../components/eventlist"), {
+  ssr: false,
+});
 
 export default function VendorDash() {
   return (
-    <div className="max-w-6xl mx-auto px-4 pb-20">
+    <div className="min-h-[100vh] flex flex-col">
       <AIChatPage role="vendor" header="Vendor Console" />
-      <div className="mt-10" />
-      <EventList />
+      <EventList role="vendor" />
     </div>
   );
 }
