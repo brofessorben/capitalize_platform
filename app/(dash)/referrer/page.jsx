696ae597894107cfd5e3ecd92bff4aac01afbc42
brofessorben <bengg2125@gmail.com>
2025-09-06 01:24:39 -0500

    Update page.jsx

--- app/(dash)/referrer/page.jsx
+++ app/(dash)/referrer/page.jsx
@@ -1,10 +1,1 @@
-"use client";
 
-import dynamic from "next/dynamic";
-
-// relative import (keeps casing exactly as in your repo)
-const AIChatPage = dynamic(() => import("../../components/aichatpage"), { ssr: false });
-
-export default function ReferrerDash() {
-  return <AIChatPage role="referrer" header="Referrer Console" />;
-}
