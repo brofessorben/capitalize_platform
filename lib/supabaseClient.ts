f0a1914d274c57c9acb2be8812127a0957311d6a
brofessorben <bengg2125@gmail.com>
2025-09-06 02:15:08 -0500

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,9 +1,8 @@
-// lib/supabaseClient.ts
 import { createClient } from "@supabase/supabase-js";
 
-const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const url  = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(url, key, {
+export const supabase = createClient(url, anon, {
   auth: { persistSession: false },
 });
