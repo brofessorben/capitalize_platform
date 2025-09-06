404a1521d5b7ebdd95af9037005f2068151919d1
brofessorben <bengg2125@gmail.com>
2025-09-06 03:41:47 -0500

    Update page.jsx

--- app/(dash)/host/page.jsx
+++ app/(dash)/host/page.jsx
@@ -1,19 +1,7 @@
 "use client";
-
 import dynamic from "next/dynamic";
-
-const AIChatPage = dynamic(() => import("../../components/aichatpage"), {
-  ssr: false,
-});
-const EventList = dynamic(() => import("../../components/eventlist"), {
-  ssr: false,
-});
+const AIChatPage = dynamic(() => import("../../components/aichatpage"), { ssr: false });
 
 export default function HostDash() {
-  return (
-    <div className="min-h-[100vh] flex flex-col">
-      <AIChatPage role="host" header="Host Console" />
-      <EventList role="host" />
-    </div>
-  );
+  return <AIChatPage role="host" header="Host Console" />;
 }
