b5d245c8cf6489cc680b7f36efb2adf396c96333
brofessorben <bengg2125@gmail.com>
2025-09-06 01:24:58 -0500

    Update page.jsx

--- app/(dash)/referrer/page.jsx
+++ app/(dash)/referrer/page.jsx
@@ -1,1 +1,15 @@
+"use client";
 
+import { useState } from "react";
+import AIChatPage from "../../components/aichatpage";
+import EventList from "../../components/eventlist";
+
+export default function ReferrerDash() {
+  const [eventId, setEventId] = useState(null);
+  return (
+    <div className="max-w-6xl mx-auto p-4 md:p-6">
+      <AIChatPage role="referrer" header="Referrer Console" eventId={eventId} />
+      <EventList role="referrer" activeId={eventId} onSelect={setEventId} />
+    </div>
+  );
+}
