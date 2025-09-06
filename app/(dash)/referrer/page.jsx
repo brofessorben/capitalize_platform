04716c1b5fc600a69d78a59176a5f768a8d5b470
brofessorben <bengg2125@gmail.com>
2025-09-06 03:41:10 -0500

    Update page.jsx

--- app/(dash)/referrer/page.jsx
+++ app/(dash)/referrer/page.jsx
@@ -1,20 +1,7 @@
 "use client";
-
 import dynamic from "next/dynamic";
-
-// client components
-const AIChatPage = dynamic(() => import("../../components/aichatpage"), {
-  ssr: false,
-});
-const EventList = dynamic(() => import("../../components/eventlist"), {
-  ssr: false,
-});
+const AIChatPage = dynamic(() => import("../../components/aichatpage"), { ssr: false });
 
 export default function ReferrerDash() {
-  return (
-    <div className="min-h-[100vh] flex flex-col">
-      <AIChatPage role="referrer" header="Referrer Console" />
-      <EventList role="referrer" />
-    </div>
-  );
+  return <AIChatPage role="referrer" header="Referrer Console" />;
 }
