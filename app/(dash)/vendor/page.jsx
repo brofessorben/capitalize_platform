42de0d8193a7b3e08d2aa8260664855e50bba03e
brofessorben <bengg2125@gmail.com>
2025-09-13 16:57:36 -0500

    Update page.jsx

--- app/(dash)/vendor/page.jsx
+++ app/(dash)/vendor/page.jsx
@@ -1,7 +1,24 @@
 "use client";
-import dynamic from "next/dynamic";
-const AIChatPage = dynamic(() => import("../../components/aichatpage"), { ssr: false });
 
-export default function VendorDash() {
-  return <AIChatPage role="vendor" header="Vendor Console" />;
+import React, { useEffect } from "react";
+import UserGate from "../../components/userGate";
+import AIChatPage from "../../components/aichatpage";
+import EventList from "../../components/eventlist";
+import { ensureProfile } from "@/lib/ensureProfile";
+
+export default function VendorPage() {
+  useEffect(() => {
+    ensureProfile();
+  }, []);
+
+  return (
+    <UserGate>
+      <div className="mx-auto max-w-6xl p-4 md:p-6 text-white">
+        <AIChatPage role="vendor" header="Vendor Console" />
+        <div className="mt-8">
+          <EventList role="vendor" />
+        </div>
+      </div>
+    </UserGate>
+  );
 }
