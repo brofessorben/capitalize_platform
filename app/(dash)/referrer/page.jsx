db158c9de97b37a44465f373eccd5873b9264c2a
brofessorben <bengg2125@gmail.com>
2025-09-13 16:57:07 -0500

    Update page.jsx

--- app/(dash)/referrer/page.jsx
+++ app/(dash)/referrer/page.jsx
@@ -1,7 +1,24 @@
 "use client";
-import dynamic from "next/dynamic";
-const AIChatPage = dynamic(() => import("../../components/aichatpage"), { ssr: false });
 
-export default function ReferrerDash() {
-  return <AIChatPage role="referrer" header="Referrer Console" />;
+import React, { useEffect } from "react";
+import UserGate from "../../components/userGate";
+import AIChatPage from "../../components/aichatpage";
+import EventList from "../../components/eventlist";
+import { ensureProfile } from "@/lib/ensureProfile";
+
+export default function ReferrerPage() {
+  useEffect(() => {
+    ensureProfile(); // creates/updates profile row for the logged in user
+  }, []);
+
+  return (
+    <UserGate>
+      <div className="mx-auto max-w-6xl p-4 md:p-6 text-white">
+        <AIChatPage role="referrer" header="Referrer Console" />
+        <div className="mt-8">
+          <EventList role="referrer" />
+        </div>
+      </div>
+    </UserGate>
+  );
 }
